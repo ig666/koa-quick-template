570674df2566e4bfb60a0dc4be0c4fe5e4150756
qi.chen <nx-7416b>
2021-01-18 13:57:34 +0800

    modify 基本框架搭建完成 解决ts-node启动问题

---
+++ src/app/app.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import * as Koa from 'koa';
+import * as cors from '@koa/cors';
+import * as body from 'koa-body';
+import * as staticService from 'koa-static';
+import * as Router from 'koa-router';
+import { autoRouter } from 'koa-route-decors';
+import { loggerHandle } from './middleware/logger.middleware';
+import { errorHandle } from './middleware/error.middleware';
+import { responseHandle } from './middleware/response.middleware';
+import { jwt } from './middleware/jwt.middleware';
+import { resolve } from 'path';
+import { connection } from './database';
+
+export class App {
+    private app: Koa;
+    constructor() {
+        this.app = new Koa;
+        this.init().catch(err => console.log(err))
+    }
+    private async init() {
+        const router = new Router
+        const subRouter = await autoRouter(resolve(__dirname, '/'));
+        router.use(subRouter.routes, jwt); // 路由添加jwt验证
+        this.app
+             .use(cors()) //跨域中间件
+             .use(loggerHandle)
+             .use(errorHandle)
+             .use(body({
+                 multipart:true
+             }))
+             .use(router.routes())
+             .use(router.allowedMethods)
+             .use(staticService(resolve(__dirname,'../../static')))
+             .use(responseHandle)
+    }
+    start(port:number){
+        this.app.listen(port,()=>{
+            connection();
+            console.log(`service start for ${port}`)
+        })
+    }
+}
